eb7ea917cc52d8bdbc07a121a8a7e7f430dab614
Karial <kazakovarial@gmail.com>
2021-05-24 14:29:25 +0300

    Database

---
+++ src/sample/DataAccessor.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class DataAccessor {
+    DataAccessor(String path) throws IOException {
+        StringTokenizer st;
+        BufferedReader TSVFile = new BufferedReader(new FileReader(path));
+        String dataRow = TSVFile.readLine();
+        this.users = new java.util.HashSet<User>();
+
+        while (dataRow != null) {
+            st = new StringTokenizer(dataRow, "\t");
+            List<String> dataArray = new ArrayList<String>();
+            while (st.hasMoreElements()) {
+                dataArray.add(st.nextElement().toString());
+            }
+            this.users.add(new User(dataArray.get(0), dataArray.get(1)));
+            dataRow = TSVFile.readLine();
+        }
+        TSVFile.close();
+    }
+
+    Boolean CheckUser(User user) {
+        return this.users.contains(user);
+    }
+
+    java.util.HashSet<User> users;
+}
---
+++ src/sample/User.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class User {
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    String login, password;
+}
